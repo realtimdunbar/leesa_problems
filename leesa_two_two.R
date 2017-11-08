a41f65f0fae156e7a22b49daf4df303e267c4517
Tim Dunbar <tim.dunbar@gmail.com>
2017-11-08 07:12:50 -0500

    cleaning up leesa_two_two

--- leesa_two_two.R
+++ leesa_two_two.R
@@ -1,6 +1,7 @@
 require(RODBC)
 require(ggplot2)
 require(jsonlite)
+require(reshape2)
 
 ##create a connection to the database
 my_connection <- odbcDriverConnect("driver={ODBC Driver 13 for SQL Server};
@@ -15,7 +16,6 @@ my_data <- sqlQuery(my_connection,
                     "SELECT *
                     FROM predictive.webapi")
 
-
 ##close connection
 odbcClose(my_connection)
 
@@ -28,20 +28,21 @@ df <- df[,grepl("entry_school_score", names(df))]
 df <- sapply(df, as.numeric)
 df_means <- t(colMeans(df, na.rm = TRUE))
 
-target <- c('october_entry_school_score',
-            'november_entry_school_score',
-            'december_entry_school_score',
-            'january_entry_school_score',
-            'february_entry_school_score',
-            'march_entry_school_score',
-            'april_entry_school_score',
-            'may_entry_school_score')
+target <- c('October',
+            'November',
+            'December',
+            'January',
+            'February',
+            'March',
+            'April',
+            'May')
 
-##convert to data frame order for plotting
-df_formatted <- data.frame(key=names(df_reordered), value=df_reordered, row.names=NULL)
+df_formatted <- data.frame(key=names(df_means[,1:8]), value=df_means[1,], row.names=NULL)
+df_formatted$key <- gsub("_entry_school_score","",df_formatted$key)
+df_formatted$key <- gsub("^(\\w)(\\w+)", "\\U\\1\\L\\2", df_formatted$key, perl = TRUE)
 df_formatted$key <- factor(df_formatted$key, levels=target)
 
 ##plot data
 ggplot(df_formatted, aes(x=key, y=value)) + geom_bar(stat="identity", width=.5, fill="#009933") + 
-  labs(x="Month", y="Average") +
+  labs(x="Month", y="Average Entry School Score") +
   theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
